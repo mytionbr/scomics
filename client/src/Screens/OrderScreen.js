e908bfd6f16b7af3c2ab52816da536094e9606fe
mytionBr <matheuss.jesus2017@gmail.com>
2021-07-15 16:38:28 -0300

    implement order payment

--- client/src/Screens/OrderScreen.js
+++ client/src/Screens/OrderScreen.js
@@ -2,10 +2,11 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { detailsOrder } from '../actions/orderAction'
+import { detailsOrder, payOrder } from '../actions/orderAction'
 import LoadingBox from '../components/LoadingBox'
 import MessageBox from '../components/MessageBox'
 import {PayPalButton} from 'react-paypal-button-v2'
+import { ORDER_PAY_RESET } from '../constants/orderConstants'
 
 export default function OrderScreen(props) {
     
@@ -14,6 +15,14 @@ export default function OrderScreen(props) {
     const orderDetails = useSelector(state => state.orderDetails)
     const { order,loading,error} = orderDetails
     const dispatch = useDispatch()
+
+    const orderPay = useSelector(state => state.orderPay)
+
+        const { 
+        loading: loadingPay, 
+        error: errorPay, 
+        success: successPay 
+    } = orderPay;
 
     useEffect(()=>{
         const addPayPalScript = async () =>{
@@ -27,7 +36,8 @@ export default function OrderScreen(props) {
             }
             document.body.appendChild(script)
         }
-        if(!order){
+        if(!order || successPay || (order && order._id !== orderId)){
+            dispatch({type:ORDER_PAY_RESET})
             dispatch(detailsOrder(orderId))
         } else {
             if(!order.isPaid){
@@ -38,10 +48,10 @@ export default function OrderScreen(props) {
                 }
             }
         }
-    },[dispatch, order, orderId, sdkReady])
+    },[dispatch, order, orderId, sdkReady, successPay])
 
-    const successPaymentHandler = ()=>{
-
+    const successPaymentHandler = (paymentResult)=>{
+        dispatch(payOrder(order,paymentResult))
     }
 
     return  loading 
@@ -158,11 +168,16 @@ export default function OrderScreen(props) {
                                 !order.isPaid && (
                                     <li>
                                         {!sdkReady ? (<LoadingBox></LoadingBox>):
-                                            (
+                                            (   <>
+                                                {errorPay && (
+                                                    <MessageBox variant="danger">{errorPay}</MessageBox>
+                                                )}
+                                                {loadingPay && <LoadingBox></LoadingBox>}
                                                 <PayPalButton 
                                                     amount={order.totalPrice}
                                                     onSuccess={successPaymentHandler}>
                                                 </PayPalButton>
+                                                </>
                                             )
                                         }
                                     </li>
--- client/src/reducers/orderReducers.js
+++ client/src/reducers/orderReducers.js
@@ -1,4 +1,4 @@
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_RESET, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS } from "../constants/orderConstants"
+import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_RESET, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_RESET, ORDER_PAY_SUCCESS } from "../constants/orderConstants"
 
 export const orderCreateReducer = (state = {},action)=>{
     switch(action.type){
@@ -30,4 +30,19 @@ export const orderDetailsReducer = (
         default:
             return state
     }
+}
+
+export const orderPayReducer = (state = {}, action) =>{
+    switch(action.type){
+        case ORDER_PAY_REQUEST:
+            return { loading: true }
+        case ORDER_PAY_SUCCESS:
+            return { laoding: false, success: true}
+        case ORDER_PAY_FAIL:
+            return { loading: false, error: action.payload}
+        case ORDER_PAY_RESET:
+            return {}
+        default:
+            return state
+        }
 }
