3056c30fcee3ef4acb93b884bca1c431853b711e
mytionBr <matheuss.jesus2017@gmail.com>
2021-09-22 16:12:42 -0300

    switch from css to scss

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store';
+//import './index.css'
+import './assets/css/main.css';
 
 ReactDOM.render(
   <Provider store={store}>
