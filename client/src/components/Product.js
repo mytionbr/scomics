3d891decc32b9a54a3e91a3a3d4b01c9032b1de4
mytionBr <matheuss.jesus2017@gmail.com>
2021-09-23 11:30:48 -0300

    customize card layout

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,27 +1,22 @@
-import React from 'react'
-import Rating from './Rating'
-import {Link} from 'react-router-dom'
+import React from "react";
+import Rating from "./Rating";
+import { Link } from "react-router-dom";
 
-export default function Product({product,id}) {
-    return (
-        <div key={id} className="card">
-          <div className="card-header">
-            <Link to={`product/${product._id}`}>
-            <img
-              className="medium"
-              src={product.image}
-              alt="procuct"
-            />
-          </Link>
-          </div>
-        
+export default function Product({ product, id }) {
+  return (
+    <Link to={`product/${product._id}`}>
+      <div key={id} className="card">
+        <div className="card-header">
+          <img className="medium" src={product.image} alt="procuct" />
+        </div>
+
         <div className="card-body">
-          <Link to={`product/${product._id}`}>
-            <h2>{product.name}</h2>
-          </Link>
+          <h2 className="card-title">{product.name}</h2>
+          <div className="fill-box" />
           <Rating rating={product.rating} hasNumReviews={false} />
           <div className="price">R$ {product.price}</div>
         </div>
       </div>
-    )
+    </Link>
+  );
 }
